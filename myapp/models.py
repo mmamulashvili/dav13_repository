aff535efa6a491f2deae99c7de304d2a5da32d9c
1 <a1@1s-MacBook-Air.local>
2025-11-17 22:28:05 +0300

    added models

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Customer(models.Model):
-    # id ავტომატურად ემატება Django-ს მიერ (AutoField)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
@@ -13,7 +12,7 @@ class Customer(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 class Product(models.Model):
-    sku = models.CharField(max_length=50, unique=True)  # stock-keeping unit
+    sku = models.CharField(max_length=50, unique=True) 
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
